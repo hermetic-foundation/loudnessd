508c4f10c5295b47415f2128aa3ab745b544aaf6
midischwarz12 <midischwarz@proton.me>
2026-09-15 09:22:01 -0500

    build: package loudnessd with Nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+  lib,
+  pkg-config,
+  pipewire,
+  rustPlatform,
+  ...
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "loudnessd";
+  version = "0.1.0";
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+  nativeBuildInputs = [
+    pkg-config
+    rustPlatform.bindgenHook
+  ];
+  buildInputs = [ pipewire ];
+
+  meta = {
+    description = "Per-application perceived-loudness controller for PipeWire";
+    license = lib.licenses.agpl3Plus;
+    platforms = lib.platforms.linux;
+    mainProgram = "loudnessd";
+  };
+}

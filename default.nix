340f5b37b8df4e8c3dfa84b9ac4f65b85357698f
midischwarz12 <midischwarz@proton.me>
2026-09-18 17:21:29 -0500

    chore: release version 0.1.1

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "loudnessd";
-  version = "0.1.0";
+  version = "0.1.1";
   src = lib.fileset.toSource {
     root = ./.;
     fileset = lib.fileset.unions [
